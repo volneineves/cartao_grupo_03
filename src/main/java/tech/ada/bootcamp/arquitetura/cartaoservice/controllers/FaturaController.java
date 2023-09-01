1f66b37b53ebcb8bc1236936aae5324b53b7e0ad
Victor Borzaquel <victorborzaquel@outlook.com>
2023-09-01 02:59:53 -0300

    fix(fatura) change path

--- src/main/java/tech/ada/bootcamp/arquitetura/cartaoservice/controllers/FaturaController.java
+++ src/main/java/tech/ada/bootcamp/arquitetura/cartaoservice/controllers/FaturaController.java
@@ -1,7 +1,12 @@
 package tech.ada.bootcamp.arquitetura.cartaoservice.controllers;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import tech.ada.bootcamp.arquitetura.cartaoservice.payloads.response.FaturaResponse;
 import tech.ada.bootcamp.arquitetura.cartaoservice.services.FaturaService;
 
@@ -24,8 +29,8 @@ public class FaturaController {
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping
-    public ResponseEntity<List<FaturaResponse>> getAllByUserId(@RequestParam UUID idUser) {
+    @GetMapping("/usuario/{id}")
+    public ResponseEntity<List<FaturaResponse>> getAllByUserId(@PathVariable("id") UUID idUser) {
         List<FaturaResponse> response = service.findAllByUsuario(idUser);
         return ResponseEntity.ok(response);
     }
